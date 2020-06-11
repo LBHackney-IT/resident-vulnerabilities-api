7ae1c511c804745965fc97e7282c89a06054e284
Renny Fadoju <rennyvbirds@yahoo.com>
2020-06-11 17:05:45 +0100

    Add infrastructure for two lambda functions
    We want to have a lambda for the API and another for the data pipeline.

--- ResidentVulnerabilitiesApi/LambdaEntryPoint.cs
+++ ResidentVulnerabilitiesApi/LambdaEntryPoint.cs
@@ -1,4 +1,5 @@
 using Amazon.Lambda.AspNetCoreServer;
+using Amazon.Lambda.Core;
 using Microsoft.AspNetCore.Hosting;
 
 namespace ResidentVulnerabilitiesApi
@@ -9,6 +10,12 @@ namespace ResidentVulnerabilitiesApi
         {
             builder
                 .UseStartup<Startup>();
+        }
+
+        // Placeholder for data pipeline lambda function
+        public void RunDataPipeline(ILambdaContext context)
+        {
+            // TO DO: Add logic for data pipeline
         }
     }
 }
